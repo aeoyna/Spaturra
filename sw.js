dde099d6278e47a73dce3a188a3b09e45b274008
aeoyna <c1304558st.kanazawa@gmail.com>
2026-02-25 20:38:23 +0900

    Add PWA support (manifest, service worker, icons)

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = 'gradius-v1';
+const ASSETS = [
+    './',
+    './index.html',
+    './css/style.css',
+    './js/main.js',
+    './js/game.js',
+    './js/player.js',
+    './js/enemy.js',
+    './js/bullet.js',
+    './js/input.js',
+    './js/audio.js',
+    './js/obstacle.js',
+    './js/gate.js',
+    './js/barrel.js',
+    './js/background.js',
+    './js/particle.js',
+    './js/option.js',
+    './manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(ASSETS);
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
